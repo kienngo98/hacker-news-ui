4295c1039cd6c831e8cc7983084c7b15a20cd994
Kien Ngo <kenseverus@gmail.com>
2022-07-10 12:41:55 -0500

    Style update

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,20 @@
 import { Post } from "../classes/Item";
-import styles from './Card.module.css';
-const Card = ({post}: {post: Post}) => {
-    return <div className={styles.Card}>
-        <div className={styles.postTitle}>{post.title}</div>
-        <div>By: <em>{post.by}</em></div>
-        <a href={post.url} target='_blank' className={styles.postUrl}>{post.url}</a>
-        <div>{post.descendants} comments | {post.score} points</div>
-    </div>
-}
+import styles from "./Card.module.css";
+const Card = ({ post }: { post: Post }) => {
+  return (
+    <a className={styles.Card} target="_self">
+      <div className={styles.postTitle}>{post.title}</div>
+      <div>
+        By: <em>{post.by}</em>
+      </div>
+      <a href={post.url} target="_blank" className={styles.postUrl}>
+        {post.url}
+      </a>
+      <div>
+        {post.descendants} comments | {post.score} points
+      </div>
+    </a>
+  );
+};
 
 export default Card;
